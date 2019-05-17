7ad6cd1986b66b2f06f230c4d63bc5cc626e6d6b
Luke Newton <28909353+LukeANewton@users.noreply.github.com>
2019-05-17 11:33:09 -0400

    rename features.py file

---
+++ ImageFeatures.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 20:10:35 2019
+
+@author: luken
+"""
+
+class Features:
+    def __init__(self, pathname, brightness, majorityRed, majorityGreen, majorityBlue):
+        self.pathname = pathname
+        self.brightness = brightness
+        self.majorityRed = majorityRed
+        self.majorityGreen = majorityGreen
+        self.majorityBlue = majorityBlue
+        
